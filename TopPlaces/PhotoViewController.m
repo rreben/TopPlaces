ab22538984b0dda77b81a97cd93d67afda20551e
rreben <rupert.rebentisch@googlemail.com>
2013-09-13 23:20:56 +0200

    Resizing of images implemented.

--- TopPlaces/PhotoViewController.m
+++ TopPlaces/PhotoViewController.m
@@ -32,7 +32,16 @@
 	NSURL * urlForPhoto = [FlickrFetcher urlForPhoto:self.photo format:FlickrPhotoFormatOriginal];
 //    UIImage * img = [[UIImage alloc] imageWithData:[NSData dataWithContentsOfURL:urlForPhoto]];
     UIImage * img = [UIImage imageWithData:[NSData dataWithContentsOfURL:urlForPhoto]];
-    [self.photoView setImage:img];
+//    NSLog(@"scale: %f",[self calculateScaleForImage:img]);
+//    [self.photoView setImage:img];
+    CGSize modSize = img.size;
+    double scale = [self calculateScaleForImage:img];
+    modSize.height = scale * modSize.height;
+    modSize.width = scale * modSize.width;
+    NSLog(@"%f", scale);
+  //  [self.photoView set]
+    self.photoView.contentMode = UIViewContentModeTopLeft;
+    [self.photoView setImage:[self imageWithImage:img convertToSize:modSize]];
 }
 
 
@@ -41,5 +50,20 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (UIImage *)imageWithImage:(UIImage *)image convertToSize:(CGSize)size {
+    UIGraphicsBeginImageContext(size);
+    [image drawInRect:CGRectMake(0, 0, size.width, size.height)];
+    UIImage *destImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return destImage;
+}
+
+- (double)calculateScaleForImage:(UIImage *)image{
+    double horizontalScale = self.photoView.bounds.size.width /  image.size.width;
+    double verticalScale = self.photoView.bounds.size.height / image.size.height;
+    return MIN(horizontalScale, verticalScale);
+}
+
 
 @end
